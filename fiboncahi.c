8284deca778259f8cc57e829ef43ea54097304a5
omandagonde99 <omandagonde99@gmail.com>
2026-05-17 09:53:32 +0530

    Add files via upload

---
+++ fiboncahi.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    int n,i,first=0,second=1,next;
+    printf("Enter the nuber of terms :");
+    scanf("%d",&n);
+
+    for(i=0;i<=n;i++){
+        // printf("%d",i);
+        // sum = sum + i;
+        printf("%d ",first);
+
+        next = first + second;
+
+        first = second;
+
+        second = next ;
+
+    }
+    return 0;
+}
